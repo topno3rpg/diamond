05f70a2776807d05bd49ccb620e1b847b52df5c1
guozhongping <guozhongping@aixuedai.com>
2016-10-28 12:01:17 +0800

    增加 长沙环境

--- diamond-server/src/test/java/com/taobao/something/JdbcPassword.java
+++ diamond-server/src/test/java/com/taobao/something/JdbcPassword.java
@@ -10,7 +10,7 @@ public class JdbcPassword {
 
     @Test
     public void encryptPassword() throws Exception {
-        System.out.println(ConfigTools.encrypt("gncuSp8kjsAtU6Wa4lo08dR"));
+        System.out.println(ConfigTools.encrypt("diamond"));
     }
 
 }
