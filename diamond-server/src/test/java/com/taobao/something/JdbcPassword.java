1f9d2c6f96515c0c5ffde57e80786c0859fc2d81
guozhongping <guozhongping@aixuedai.com>
2016-12-09 10:58:06 +0800

    增加 性能测试环境

--- diamond-server/src/test/java/com/taobao/something/JdbcPassword.java
+++ diamond-server/src/test/java/com/taobao/something/JdbcPassword.java
@@ -10,7 +10,7 @@ public class JdbcPassword {
 
     @Test
     public void encryptPassword() throws Exception {
-        System.out.println(ConfigTools.encrypt("diamond2cs"));
+        System.out.println(ConfigTools.encrypt("123456"));
         // C66J/kMf2tMh19D847AgzPLbKVJips90B5lJDlda31Q52S+tIwQnveLNztGlqlbfmb07Bb5TFTCmZ7/fYMMHcA==
 //        System.out.println(ConfigTools.decrypt("MbCY4VVd3zttNK1OI0rLa8XvtaL9Ab7sbdDsrTz6kCv1IOPzStXWiL0LaMWTSGJOdthMHDckzjWp20ufGLoGPw=="));
     }
